289c0cc231e552c9adb7c8ca3ca13e1215e1dc5f
PeporBanga <romanoff.alexand2014@gmail.com>
2025-01-12 15:17:23 +0500

    Add files via upload

---
+++ Lesson 101/main.js
@@ -0,0 +1,54 @@
+
+// const users = ['Dan', 'Denis', 'Petr', 'Alex']
+
+
+
+// function checkForName(aro, item) {
+//     for (let i = 0; i < aro.length; i++) {
+//         if (aro[i] === item) {
+//             return `Hello ${item}`
+//         }
+//     }
+//     return `The user was not found`
+// }
+
+
+// console.log(checkForName(users, 'Denis' ))
+
+
+// const number = [10, 20, 5, 14, 15, 72]
+// function checkForNumber(){
+//     for (let i = 0; i <= 10; i = i + 1 ){
+//         if (i > 10) 
+// }
+// }
+
+// console.log(checkForNumber() )
+
+
+
+
+// function calc ( number1, operator, number2){
+//     if (operator == 'minus'){
+//         return number1 - number2
+//     }
+//     if (operator == 'sum'){
+//         return number1 + number2
+//     }
+//     if (operator == 'multiplication'){
+//         return number1 * number2
+//     }
+//     if (operator == 'division'){
+//         return number1 / number2
+//     }
+// }
+
+// const result = calc(3, 'minus', 110)
+// console.log (result)
+// console.log(calc(100, 'minus', 50))
+// console.log(calc(100, 'sum', 50))
+// console.log(calc(100, 'multiplication', 50))
+// console.log(calc(100, 'division', 50))
+
+
+
