7900bd57a0bb29f0f4b6d1eb605e0eb668b19b95
PeporBanga <romanoff.alexand2014@gmail.com>
2025-01-13 13:03:23 +0500

    Add files via upload

---
+++ Lesson 102/main.js
@@ -0,0 +1,119 @@
+// const users = {
+//     alex: {
+//     age: 23,
+//     isAdmin: false
+//     },
+//     john: {
+//         age: 20,
+//         isAdmin: true,
+//         sayHello(name){
+//             console.log(`Hello ${name}`)
+//         }
+//     }
+// }
+
+
+
+// console.log(users.alex.isAdmin)
+// users.john.sayHello('Tom')
+
+
+// const users = [
+//     {
+//         name: 'alex',
+//         age: 23,
+//         isAdmin: false
+//     },
+//     {
+//         name: 'john',
+//         age: 18,
+//         isAdmin: true
+//     }
+// ]
+
+// users.push(
+//     {
+//         name: 'ivan',
+//         age: 23,
+//         isAdmin: false
+//     }
+// )
+
+
+// for(let i = 0; i < users.length; i++){
+//     console.log(users[i])
+// }
+
+
+// const foo = 'hello world'
+// console.log(foo.toUpperCase())
+
+// =========================================================================================================
+
+// const user = {
+//     name: 'Daniil',
+//     age: 23,
+//     country: 'Russia',
+//     city: 'Ekaterinburg',
+//     animal: 'cat',
+//     sayHello(name){
+//         console.log(`Hello ${name}`)
+//     }
+
+// }
+
+
+
+// console.log(user)
+// user.sayHello('Daniil')
+
+
+// ============================================\
+
+
+const users = [
+    {
+        name: 'daniil',
+        age: 23,
+        country: 'Russia',
+        city: 'Ekaterinburg',
+        animal: 'cat',
+        isAdmin: true
+    },
+    {
+        name: 'max',
+        age: 25,
+        country: 'Russia',
+        city: 'Ekaterinburg',
+        animal: 'dog',
+        isAdmin: false
+    },
+    {
+        name: 'denis',
+        age: 20,
+        country: 'Russia',
+        city: 'Ekaterinburg',
+        animal: '',
+        isAdmin: false
+    },
+    {
+        name: 'tom',
+        age: 48,
+        country: 'Russia',
+        city: 'Ekaterinburg',
+        animal: 'mous',
+        isAdmin: true
+    }
+]
+let simple;
+simple = 0
+function checkFor(array, item)
+{
+for(let i = 0; i < array.length; i++){
+    if(array[i] === item)
+        return  (simple = simple + 1)
+}
+
+}
+console.log(checkFor(users, false))
+// 
